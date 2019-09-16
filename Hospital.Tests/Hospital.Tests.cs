7c696735c1bbfacfd172ceb0579393f40eaff4a5
Andy Kohler <andykohler99@gmail.com>
2019-09-16 16:50:28 -0400

    Added Employee and Patient Classes

--- Hospital.Tests/Hospital.Tests.cs
+++ Hospital.Tests/Hospital.Tests.cs
@@ -6,9 +6,52 @@ namespace Hospital.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Doctor_Makes_Ninety_Thousand()
         {
+            // Act
+            Employee doctor = new Employee();
+            doctor.Salary = 90000;
+
+            //Arrange
+
+            doctor.Doctor();
+
+            //Assert
+
+            Assert.Equal(90000, doctor.Salary);
+        }
+        [Fact]
+        public void Is_Doctor_Paid()
+        {
+            // Act
+            Employee doctor = new Employee();
+            doctor.IfPaid = false;
+
+            //Arrange
+
+            doctor.Doctor();
+
+            //Assert
+
+            Assert.False(false);
+        }
+        [Fact]
+        public void Is_NurseID_002()
+        {
+    
+            // Act
+            Employee nurse = new Employee();
+            nurse.NumberID = 002;
+
+            //Arrange
+
+            nurse.Nurse();
+
+            //Assert
+
+            Assert.Equal(002, nurse.NumberID);
 
         }
+
     }
 }
--- Hospital/Employee.cs
+++ Hospital/Employee.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Hospital
 {
-    class Employee
+    public class Employee
     {
+        public string Name { get; set; }
+        public int NumberID { get; set; }
+        public int Salary { get; set; }
+        public bool IfPaid { get; set; }
+        
+        public void Doctor() 
+        {
+            Name = "john";
+            NumberID = 001;
+            Salary = 90000;
+            IfPaid = false;
+        }
+        public void Nurse()
+        {
+            Name = "mary";
+            NumberID = 002;
+            Salary = 50000;
+            IfPaid = false;
+        }
     }
 }
